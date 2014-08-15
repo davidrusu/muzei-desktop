e2bab41baece484f65665c526b7f160c3cdc8a72
David Rusu <davidrusu@gmail.com>
2014-08-15 20:49:03 +0200

    parsing json

--- muzei.hs
+++ muzei.hs
@@ -1,11 +1,26 @@
-import Network.Curl.Download
-import Text.JSON
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE OverloadedStrings #-}
 
+import Network.Curl.Download
+import Data.Aeson (FromJSON, ToJSON, decode, encode)
+import qualified Data.ByteString.Lazy.Char8 as BL
+
+import GHC.Generics (Generic)
+
+data ImageDetails = ImageDetails {
+      byline     :: String, -- "Pierre-Auguste Renoir, 1881"
+      detailsUri :: String,           -- "http://www.wikiart.org/en/pierre-auguste-renoir/the-luncheon-of-the-boating-party-1881-1?utm_source=Muzei&utm_campaign=Muzei"
+      imageUri   :: String,           -- "http://storage.googleapis.com/muzeifeaturedart/fullres/the-luncheon-of-the-boating-party-pierre-auguste-renoir-1881.jpg"
+      nextTime   :: String,           -- "2014-08-16T02:00:00+00:00"
+      thumbUri   :: String,           -- "http://storage.googleapis.com/muzeifeaturedart/thumbs/the-luncheon-of-the-boating-party-pierre-auguste-renoir-1881.jpg"
+      title      :: String            -- "The Luncheon of the Boating Party"
+    } deriving (Show, Generic)
+
+instance FromJSON ImageDetails
 main = do
   doc <- openURIString "http://muzeiapi.appspot.com/featured?cachebust=1"
-  let json = getJsonResult doc
-  putStrLn $ show json
+  what doc
 
-getJsonResult :: (JSON a) => Either String String -> Result a
-getJsonResult Left msg = Error msg
-getJsonResult Right js = decode js
+what :: Either String String -> IO ()
+what (Left errMsg) = print errMsg
+what (Right result) = print $ (decode $ BL.pack result :: Maybe ImageDetails)
